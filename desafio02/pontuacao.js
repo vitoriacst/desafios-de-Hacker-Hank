f900578ea2dc776de85d04398a4e085ac4b52c50
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-05-07 01:32:56 -0300

    desafio 2

--- desafio02/pontuacao.js
+++ desafio02/pontuacao.js
@@ -1,24 +1,15 @@
 //ana -> [0,1,2]
 //bob ->[1,0,3]
-const points = () => {
-    let bobArray = [1, 3, 5];
-    let alicePoints = 0
-    let aliceArray = [0, 1, 3];
-    for (let index = 0; index < aliceArray.length; index++) {
-      let bobPoints = 0;
-      for (let index = 0; index < bobArray.length; index++) {
-        if (bobArray[0] > aliceArray[0]) {
-          return (bobPoints += 1);
-        }
-        if (bobArray[1] > aliceArray[1]) {
-          return (bobPoints += 1);
-        }
-        if (bobArray[2] > aliceArray[2]) {
-          return (bobPoints += 1);
-        }
-        console.log([alicePoints+''+bobPoints]);
-      }
+function compareTriplets(a, b) {
+  let alicePoints = 0
+  let bobPoints = 0;
+for(let index = 0; index < 3; index++) {
+    if (b[index] < a[index]) {
+       (alicePoints += 1);
     }
-  };
-  points();
-  
+    if (b[index] > a[index]) {
+       (bobPoints += 1);
+    }
+  }
+    return([alicePoints,bobPoints]);
+}
